ee0f2c98f2fdff967ba4a067fe32f830001900f8
Shwetabh Niket <f20210923@pilani.bits-pilani.ac.in>
2022-11-28 18:21:29 +0530

    feat: add svg on carousel

--- src/Components/HomeCaroItem.js
+++ src/Components/HomeCaroItem.js
@@ -8,7 +8,7 @@ function HomeCaroItem(props) {
     <Box height="100%" textAlign={"left"}  color="#EEE3D3"  backgroundImage={Mark} backgroundSize="cover">
         <Box pl="2rem" height="100%" w="100%" background="linear-gradient(357.91deg, #000000 1.84%, rgba(0, 0, 0, 0) 61.55%);">
         <Box pt="11rem" fontSize={"1.6rem"} fontWeight={"600"}>IBW</Box>
-        <Box fontSize={"0.8rem"} display={"inline"}>TURKEY 2022</Box>
+        <Flex fontSize={"0.8rem"} alignItems="center">TURKEY <BsDot fontSize="2rem"/> 2022</Flex>
         </Box>
     </Box>
   )
