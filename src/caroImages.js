0ab00400b65dfad24977b39e7cd8f49484d0707e
Shwetabh Niket <f20210923@pilani.bits-pilani.ac.in>
2022-12-01 00:31:15 +0530

    feat: add content on copies

--- src/caroImages.js
+++ src/caroImages.js
@@ -1,7 +1,7 @@
 export const images = [
-    {heading: "A pumped", source: "../Assets/one.gif", subHeading: "My eyes glowed when I was in the 7th standard, I wanted to be in the ecosystem."},
-    {heading: "B pumped", source: "../Assets/2.gif", subHeading: "My eyes glowed when I was in the 7th standard, I wanted to be in the ecosystem."},
-    {heading: "C pumped", source: "../Assets/3.gif", subHeading: "My eyes glowed when I was in the 7th standard, I wanted to be in the ecosystem."},
-    {heading: "D pumped", source: "../Assets/4.gif", subHeading: "My eyes glowed when I was in the 7th standard, I wanted to be in the ecosystem."},
+    {heading: "Blood of an entrepreneur", source: "../Assets/one.gif", subHeading: "The Social Network and Silicon Valley sowed a seed in me, one that’s got firm roots now."},
+    {heading: "Eye of a designer", source: "../Assets/2.gif", subHeading: "I experiment and explore. Push a few boundaries and then some more."},
+    {heading: "Wit of a playmaker", source: "../Assets/3.gif", subHeading: "Identifying synergies and realising the full potential of a team."},
+    {heading: "Legs of a wanderer", source: "../Assets/4.gif", subHeading: "How can I sit at home, when there are so many places and so many faces that I am yet to see."},
     // {heading: "E pumped", subHeading: "My eyes glowed when I was in the 7th standard, I wanted to be in the ecosystem."},
 ]
